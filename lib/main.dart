d8e864fe12e7dc4c7171e6bd6d08543416efedf3
Aleksa Marinkovic <aleksa13.am@gmail.com>
2023-10-10 11:30:17 +0200

    PartnerPage fertiggestellt

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(MyApp());
 }
 
@@ -41,6 +47,7 @@ class _MainPageState extends State<MainPage> {
         centerTitle: true,
       ),
       body: _pages[_currentIndex],
+      
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color.fromRGBO(65, 130, 69, 100),
         currentIndex: _currentIndex,
@@ -75,6 +82,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text('Inhalt der HomePage'),
+      
     );
   }
 }
@@ -89,92 +97,145 @@ class _DataPageState extends State<DataPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          DropdownButton<String>(
-            value: selectedOption,
-            onChanged: (String? value) {
-              setState(() {
-                selectedOption = value;
-              });
-            },
-            items: <String>[
-              'Option 1',
-              'Option 2',
-              'Option 3',
-              'Option 4',
-              'Option 5',
-              'Option 6',
-            ].map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
-          Padding(padding: EdgeInsets.only(top: 60)),
-          const Card(
-            color: Color.fromRGBO(225, 225, 225, 100),
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              side: BorderSide(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(12)),
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            DropdownButton<String>(
+              value: selectedOption,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedOption = value;
+                });
+              },
+              items: <String>[
+                'Option 1',
+                'Option 2',
+                'Option 3',
+                'Option 4',
+                'Option 5',
+                'Option 6',
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
-            child: SizedBox(
-              width: 270,
-              height: 100,
-              child: Center(
-                  child: Text(
-                "Temp:",
-                style: TextStyle(fontSize: 20),
-              )),
+            Padding(padding: EdgeInsets.only(top: 60)),
+            const Card(
+              color: Color.fromRGBO(225, 225, 225, 100),
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: SizedBox(
+                width: 270,
+                height: 100,
+                child: Center(
+                    child: Text(
+                  "Temp:",
+                  style: TextStyle(fontSize: 20),
+                )),
+              ),
             ),
-          ),
-          Padding(padding: EdgeInsets.only(top: 20)),
-          const Card(
-            color: Color.fromRGBO(225, 225, 225, 100),
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              side: BorderSide(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(12)),
+            Padding(padding: EdgeInsets.only(top: 20)),
+            const Card(
+              color: Color.fromRGBO(225, 225, 225, 100),
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: SizedBox(
+                width: 270,
+                height: 100,
+                child: Center(
+                    child: Text("Luftfeuchte:", style: TextStyle(fontSize: 20))),
+              ),
             ),
-            child: SizedBox(
-              width: 270,
-              height: 100,
-              child: Center(
-                  child: Text("Luftfeuchte:", style: TextStyle(fontSize: 20))),
+            Padding(padding: EdgeInsets.only(top: 20)),
+            const Card(
+              color: Color.fromRGBO(225, 225, 225, 100),
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: SizedBox(
+                width: 270,
+                height: 100,
+                child: Center(
+                    child: Text(
+                  "CO2:",
+                  style: TextStyle(fontSize: 20),
+                )),
+              ),
             ),
-          ),
-          Padding(padding: EdgeInsets.only(top: 20)),
-          const Card(
-            color: Color.fromRGBO(225, 225, 225, 100),
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              side: BorderSide(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-            ),
-            child: SizedBox(
-              width: 270,
-              height: 100,
-              child: Center(
-                  child: Text(
-                "CO2:",
-                style: TextStyle(fontSize: 20),
-              )),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
 class PartnerPage extends StatelessWidget {
+  goToWebPage(String urlString) async {
+   if(await canLaunch(urlString)){
+    await launch(urlString);
+   } else {
+    throw 'Webseite $urlString konnte nicht geladen werden';
+   }
+}
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Inhalt der Partnerseite'),
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+              iconSize: 150,
+              icon: Image.asset(
+                'images/TGM_Logo.png',
+              ),
+              onPressed: () async {
+                await goToWebPage('https://www.tgm.ac.at'); // Hier fügst du den Link zur Webseite hinzu
+              },
+            ),
+            IconButton(
+              iconSize: 150,
+              icon: Image.asset(
+                'images/BMBWF_Logo_srgb.png',
+                
+              ),
+              onPressed: () async {
+                await goToWebPage('https://www.bmbwf.gv.at'); // Hier fügst du den Link zur Webseite hinzu
+              },
+            ),
+            IconButton(
+              iconSize: 150,
+              icon: Image.asset(
+                'images/TU-Logo-Austria_CMYK.png',
+              ),
+              onPressed: () async{
+                await goToWebPage('https://www.tuwien.at'); // Hier fügst du den Link zur Webseite hinzu
+              },
+            ),
+            IconButton(
+              iconSize: 150,
+              icon: Image.asset(
+                'images/OeAD_LogoUnterzeile_DE_RGB.png',
+              ),
+              onPressed: () async {
+                await goToWebPage('https://oead.at'); // Hier fügst du den Link zur Webseite hinzu
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
